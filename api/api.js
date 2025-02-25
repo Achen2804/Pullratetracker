26ccb2ce63a3c01e2bf198c69978c98c2880e6dc
andrew <andrewchen200406@gmail.com>
2025-02-25 16:02:36 -0500

    Added API call functionality

---
+++ api/api.js
@@ -0,0 +1,24 @@
+module.exports = async (req, res) => {
+    const time = new Date().getDay();
+    const targetUrl = ""; 
+    if(time <15){
+        targetUrl = "https://pullratetracker.onrender.com/";
+    }else{
+        targetUrl = "https://pullratetracker-1.onrender.com"
+    }
+  
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: req.method,  
+      headers: req.headers, 
+      body: req.method === 'POST' || req.method === 'PUT' ? req.body : undefined, 
+    });
+
+    const data = await response.json(); // Assuming the response is JSON
+    res.status(response.status).json(data); // Send the response back to the client
+  } catch (error) {
+    console.error('Error forwarding request:', error);
+    res.status(500).json({ error: 'Failed to forward request' });
+  }
+};
