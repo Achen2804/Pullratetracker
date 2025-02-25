1086facb4ef5869ad99d483c1cfe52ccec7eab92
andrew <andrewchen200406@gmail.com>
2025-02-25 16:55:28 -0500

    Changed my flask app

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+from pokemontcgsdk import Type
+from pokemontcgsdk import Supertype
+from pokemontcgsdk import Subtype
+from pokemontcgsdk import Rarity
+
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend access
@@ -10,7 +18,15 @@ def home():
 
 @app.route('/api/data')
 def get_data():
-    return jsonify({"message": "Hello from Flask!", "status": "success"})
+    url = "https://api.pokemontcg.io/v2/cards/"
+    headers = {
+        "X-Api-Key": "3ba6f406-4bd1-4b37-ad93-64fef7a956d3"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    card = Card.find('sv8pt5-16')
+    return jsonify({card})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
