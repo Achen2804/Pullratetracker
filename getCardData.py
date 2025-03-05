7b858b21d6c3e7fef5172d8db7703f645d366f43
andrew <andrewchen200406@gmail.com>
2025-03-04 21:08:26 -0500

    Updated webscraper to be more robust.

--- getCardData.py
+++ getCardData.py
@@ -5,15 +5,32 @@ from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
+from pokemontcgsdk import RestClient
 
-url = "https://api.pokemontcg.io/v2/cards/"
-headers = {
-    "X-Api-Key": "redacted"
-}
+RestClient.configure('3ba6f406-4bd1-4b37-ad93-64fef7a956d3')
 
-response = requests.get(url, headers=headers)
-data = response.json()
-card = Card.find('sv8pt5-16')
-print(data)
-# Process the data
-#print(card)
+def process_set(set_name):
+    set = Set.find(set_name)
+    set_json = set.images.logo
+    images = []
+    images.append(set_json)
+    cards = Card.where(q='(set.id:swsh12 OR set.id:swsh12tg) rarity:"Rare Ultra"')
+    card_ids = []
+    for card in cards:
+        card_ids.append(card.id)
+        images.append(card.images.large)
+    print(card_ids)
+    return images
+
+images = process_set('swsh1')
+print(Card.find('swsh12pt5gg-GG05').rarity)
+#print(Rarity.all())
+html_content = "<html><body>" + "".join(f'<img src="{url}" style="width:300px;"><br>' for url in images) + "</body></html>"
+
+with open("images.html", "w") as f:
+    f.write(html_content)
+
+print("Open images.html in your browser")
+
+
+
