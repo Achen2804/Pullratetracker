dfe5cabf864abc618feffe2d2b3c58d91453827c
andrew <andrewchen200406@gmail.com>
2025-03-12 17:12:21 -0400

    Added searching feature to the flask app. I can now return images of pokemon based on their rarities.

--- Render_flask/flaskapp.py
+++ Render_flask/flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 from pokemontcgsdk import Card
@@ -7,7 +7,9 @@ from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
+from pokemontcgsdk import RestClient
 
+RestClient.configure('3ba6f406-4bd1-4b37-ad93-64fef7a956d3')
 
 app = Flask(__name__)
 CORS(app)
@@ -22,14 +24,24 @@ def send_message():
 
 @app.route('/api/getcard')
 def get_data():
-    url = "https://api.pokemontcg.io/v2/cards/"
-    headers = {
-        "X-Api-Key": "3ba6f406-4bd1-4b37-ad93-64fef7a956d3"
-    }
-
-    response = requests.get(url, headers=headers)
-    data = response.json()
     card = Card.find('sv8pt5-16')
-    return jsonify(card)
+    return jsonify(card.images.large)
+
+@app.route('/api/getset')
+def get_set():
+    set_name = request.args.get('set_name','None',type=str)
+    print(f"Received set_name: {set_name}")
+    if(set_name == 'None'):
+        return jsonify({'error':'Something went wrong and no set name was provided. Please try again'})
+    poke_set = Set.find(set_name)
+    set_json = poke_set.images.logo
+    images = []
+    images.append(set_json)
+    cards = Card.where(q=f'(set.id:{set_name} OR set.id:{set_name}tg) rarity:"Rare Ultra"')
+    card_ids = []
+    for card in cards:
+        card_ids.append(card.id)
+        images.append(card.images.large)
+    return jsonify(images)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
